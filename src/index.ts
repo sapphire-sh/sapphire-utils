1b9d12df416eb1636a80c707860f56c5a7cadb5c
sapphire <info@sapphire.sh>
2022-08-18 07:52:37 +0900

    add getEnumValue

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+export const getEnumValue = <T extends string, TEnum extends string>(value: { [key in T]: TEnum }) => {
+	const enumValues = Object.values(value);
+	return (value?: string): TEnum | null => (enumValues.includes(value) ? (value as TEnum) : null);
+};
+
 export const isNonNullable = <T extends object>(value?: T | null): value is NonNullable<T> => {
 	if (value === null) {
 		return false;
