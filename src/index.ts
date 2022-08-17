56c1d5a164f641baf96c017060092a723b64ec0e
sapphire <info@sapphire.sh>
2022-08-18 07:36:41 +0900

    add isNonNullable

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+export const isNonNullable = <T extends object>(value?: T | null): value is NonNullable<T> => {
+	if (value === null) {
+		return false;
+	}
+	if (value === undefined) {
+		return false;
+	}
+	return true;
+};
+
 export const sleep = (ms: number): Promise<void> => {
 	return new Promise((resolve) => setTimeout(resolve, ms));
 };
